5a1777dd5094a83fccc633674b2c0d366a6a4b19
Ivan Ristic <iristic979@gmail.com>
2023-02-17 01:40:36 +0100

    Start batch file (no model, from scratch)

--- zStart.bat
+++ zStart.bat
@@ -1,1 +1,1 @@
-python train.py --data ../datasets/labels3/labels3.yaml --weights yolov5n.pt --img 640 --epochs 1 --name labels3
+python train.py --data ../datasets/labels3/labels3.yaml --weights '' --img 640 --epochs 1 --name labels3
